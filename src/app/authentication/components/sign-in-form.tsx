945e4267c65faa781278df89d72a0fb51d488c5c
marquezin <gabrielmarques2003@gmail.comgit config --global user.email gabrielmarques2003@gmail.comgit config --global user.name marquezzinclear>
2025-08-05 22:00:56 -0300

    :spakrles: feat: add ui for sign in and sign up form components

---
+++ src/app/authentication/components/sign-in-form.tsx
@@ -0,0 +1,102 @@
+"use client"; // Por usar hook do react
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const formSchema = z.object({
+  email: z.email("E-mail Inválido!"),
+  password: z
+    .string("Senha inválida!")
+    .min(8, "A senha necessita ter no mínimo 8 digitos"),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+const SignInForm = () => {
+  // 1. Define your form.
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: FormValues) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log("Formulário valido e enviado");
+    console.log(values);
+  }
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Entrar</CardTitle>
+          <CardDescription>Faça login para continuar</CardDescription>
+        </CardHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <CardContent className="grid gap-6">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Digite sua senha"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button type="submit">Entrar</Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </>
+  );
+};
+
+export default SignInForm;
