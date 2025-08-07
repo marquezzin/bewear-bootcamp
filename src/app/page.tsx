c2875f07e4abf77b908f28bebceedac5ee4426b7
marquezin <gabrielmarques2003@gmail.comgit config --global user.email gabrielmarques2003@gmail.comgit config --global user.name marquezzinclear>
2025-08-06 23:38:02 -0300

    :sparkles: feat: add sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components/common/header";
 
 function Home() {
   return (
-    <div>
-      <h1 className="my-2 text-3xl font-bold text-red-500 underline">
-        Hello World
-      </h1>
-      <Button>Click me</Button>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
